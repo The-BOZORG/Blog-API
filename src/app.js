a023b64edd8d8e397e91b8eb5c089e5645feec8d
soroush <soroushfalaki24@hotmail.com>
2026-04-19 14:35:52 +0330

    cors - 404

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,8 +11,27 @@ const app = express();
 import config from './configs/index.js';
 import { logger } from './lib/winstone.js';
 import limiter from './lib/limiter.js';
+import notFound from './middlewares/404.js';
+
+//core
+const corsOptions = {
+  origin(origin, callback) {
+    if (
+      config.NODE_ENV === 'development' ||
+      !origin ||
+      config.WHITELIST_ORIGINS.includes(origin)
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS error : ${origin} is not allow by CORS`), false);
+      logger.warn(`CORS error :${origin} is not allow by CORS`);
+    }
+  },
+  credentials: true,
+};
 
 //middlewares
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -24,4 +44,5 @@ app.use(
 app.use(helmet());
 app.use(limiter());
 //route
+app.use(notFound);
 export default app;
