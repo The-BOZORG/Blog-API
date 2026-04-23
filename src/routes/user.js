cbe1a6c83346ec460358fb8db563a715c6a9aac2
soroush <soroushfalaki24@hotmail.com>
2026-04-23 11:41:04 +0330

    get user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
-import { body, cookie } from 'express-validator';
+import { body, cookie, param } from 'express-validator';
 import { Router } from 'express';
 
 const router = Router();
 
 import getCurrentUser from '../controllers/user/get-current-user.js';
+import getUser from '../controllers/user/get-user.js';
 
 import authorize from '../middlewares/authorize.js';
 import validationError from '../middlewares/validation-error.js';
@@ -14,6 +15,15 @@ router.get(
   authenticate,
   authorize('admin', 'user'),
   getCurrentUser,
+);
+
+router.get(
+  '/:userId',
+  authenticate,
+  authorize('admin'),
+  param('userId').notEmpty().isMongoId().withMessage('invalid user id'),
+  validationError,
+  getUser,
 );
 
 export default router;
