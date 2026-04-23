0e9f4276b7f0c9982826cabe3192c0bb83632d80
soroush <soroushfalaki24@hotmail.com>
2026-04-23 11:56:36 +0330

    get all users route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
-import { body, cookie, param } from 'express-validator';
+import { body, cookie, param, query } from 'express-validator';
 import { Router } from 'express';
 
 const router = Router();
 
 import getCurrentUser from '../controllers/user/get-current-user.js';
 import getUser from '../controllers/user/get-user.js';
+import getAllUsers from '../controllers/user/get-all-user.js';
 
 import authorize from '../middlewares/authorize.js';
 import validationError from '../middlewares/validation-error.js';
@@ -15,6 +16,21 @@ router.get(
   authenticate,
   authorize('admin', 'user'),
   getCurrentUser,
+);
+
+router.get(
+  '/',
+  authenticate,
+  authorize('admin'),
+  query('limit')
+    .optional()
+    .isInt({ min: 1, max: 20 }.withMessage('limit must be between 1 to 50')),
+  query('offset')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('offset must positive int'),
+  validationError,
+  getAllUsers,
 );
 
 router.get(
